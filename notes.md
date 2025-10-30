d58a59975157b42aaf99f1f0aca3208cc4dcac71
Nate Vance <Natevance01@gmail.com>
2025-10-30 12:22:17 -0500

    Initial arch config

---
+++ notes.md
@@ -0,0 +1,8 @@
+
+
+Arch Installation
+
+System level packages are managed by pacman / yay
+
+Develpoment Level packages are managed by nixpks
+
---
+++ scripts/install.sh
@@ -0,0 +1,20 @@
+
+-- System Packages
+sudo pacman -S git base-devel neovim
+
+
+-- Install yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ~/ 
+rm -rf yay
+
+-- Nix package manageer
+sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon
+
+-- Browser, Password Manager
+yay -S zen-browser-bin bitwarden-bin
+
+-- Clipboard Utilities
+sudo pacman -S wl-clipboard
